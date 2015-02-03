5678a962e347d93011f31e9b85c07eb48a33b1f2
Baby518 <ZhangCharles518@gmail.com>
2015-02-04 00:49:47 +0800

    Initial Commit

---
+++ RSSReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RSSReader
+//
+//  Created by zhangchao on 15/2/4.
+//  Copyright (c) 2015年 zhangchao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ RSSReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSReader
+//
+//  Created by zhangchao on 15/2/4.
+//  Copyright (c) 2015年 zhangchao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
